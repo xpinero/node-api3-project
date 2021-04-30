70b92df8f1a018686a15e3ed62bf3f46436f6dea
Xaver Pinero <xaver.pinero@gmail.com>
2021-04-30 19:35:10 -0400

    setup serv and index

--- api/server.js
+++ api/server.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
+
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).send(`<h2>Let's write some middleware! YAY!</h2>`);
+});
+
+server.use(function (req, res) {
+  res.status(404).send(`Ain't nobody got time for dat! Give me a real page!`);
 });
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server');
+
+const port = 8000;
+
+server.listen(port, () => {
+  console.log('Message in the terminal. Server started on localhost: 8000')
+})
