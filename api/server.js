9d83ffd70ef7958b9e32e5188758ad03c6cd7247
Xaver Pinero <xaver.pinero@gmail.com>
2021-05-13 21:56:28 -0400

    MVP minus last post request

--- api/server.js
+++ api/server.js
@@ -1,14 +1,33 @@
 const express = require('express');
-
+// const morgan = require('morgan');
+const { logger } = require('./middleware/middleware');
+// const deny = require('./middleware/deny');
+const usersRouter = require('./users/users-router');
 const server = express();
 
 server.use(express.json());
+// server.use(deny())
+server.use(logger);
+server.use('/api/users', usersRouter);
+// install the morgan middleware using the combined format
+//     server.use(morgan("combined"))
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
 
 
+// function auth(req, res, next) {
+//   if (req.url === '/mellon') {
+//     next();
+//   } else {
+//     res.send('You shall not pass!');
+//   }
+// }
+
+
+// server.get('/mellon', auth, (req, res) => {
+//   console.log('Gate opening...');
+//   console.log('Inside and safe!');
+//   res.send('Welcome Traveler!');
+//});
 
 server.get('/', (req, res) => {
   res.status(200).send(`<h2>Let's write some middleware! YAY!</h2>`);
