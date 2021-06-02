5262ad0e362b6c733ee666c331e90c59b7c53238
Xaver Pinero <xaver.pinero@gmail.com>
2021-06-01 21:02:44 -0400

    added env variable

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // require your server and launch it
 const server = require('./api/server');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`Message in the terminal. Server started on localhost: ${port}`)
