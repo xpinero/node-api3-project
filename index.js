e3bb8a4635b0aec337ac057788b65183ab783188
Xaver Pinero <xaver.pinero@gmail.com>
2021-06-10 20:57:04 -0400

    added cors and mods to index

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-require('dotenv').config();
+const dotenv = require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
 
 // require your server and launch it
 const server = require('./api/server');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9000;
+
+server.use(cors());
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
+server.use("/api/",(_,res)=>{
+  res.json({data:"The API is serving data!!!"})
+})
+
 
 server.listen(port, () => {
   console.log(`Message in the terminal. Server started on localhost: ${port}`)
 })
+
+server.use("*",(_,res)=>{
+  res.sendFile(path.join(__dirname,"client/build","index.html"))
+})
+
+
+console.log("It's alive!!!!")
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USER)
+console.log(process.env.PORT)
+console.log(process.env.FUN_FACT)
+console.log(process.env.FOOD)
+console.log(process.env.COHORT)
